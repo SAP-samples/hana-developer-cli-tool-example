b99533f6c6c62e3591ab736bf0b268b4e6462cf5
Thomas Jung <thomas.jung@sap.com>
2026-02-11 16:02:27 -0500

    refactor: improve documentation and structure of application initialization in init.js

--- app/resources/init.js
+++ app/resources/init.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-undef */
 /*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0, no-shadow:0 */
 /*eslint-env es6 */
-sap.ui.require(["sap/ui/core/Core", "sap/ui/core/Component"], (oCore, Component) => {
 
-    sap.ushell.Container.createRenderer().placeAt('content')
+/**
+ * Application initialization
+ * Sets up the SAPUI5 shell renderer and configures flexibility services for SAP UI
+ */
+sap.ui.require(["sap/ui/core/Core", "sap/ui/core/Component"], () => {
+    // Render the Fiori shell container
+    sap.ushell.Container.createRenderer().placeAt("content");
+    
+    // Configure flexibility services for UI adaptations
     sap.ui
         .getCore()
         .getConfiguration()
         .setFlexibilityServices([{
             connector: "SessionStorageConnector"
-        }])
-
-})
+        }]);
+});
