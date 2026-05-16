198ab699c6468f0da82472aa07b5ec79ed3ec587
Thomas Jung <thomas.jung@sap.com>
2026-05-16 19:00:09 -0400

    feat(analytics): assemble Analytics view with all three tabs

--- app/vue/src/views/Analytics.vue
+++ app/vue/src/views/Analytics.vue
@@ -2,19 +2,28 @@
 import '@ui5/webcomponents/dist/TabContainer.js'
 import '@ui5/webcomponents/dist/Tab.js'
 import { ref } from 'vue'
+import ExploreTab from '../components/analytics/ExploreTab.vue'
+import SqlTab from '../components/analytics/SqlTab.vue'
+import DashboardTab from '../components/analytics/DashboardTab.vue'
 
 const activeTab = ref('explore')
+
+function onTabSelect(e: any) {
+  activeTab.value = e.detail.tab.dataset.key
+}
 </script>
 
 <template>
   <div class="analytics-view">
-    <ui5-tabcontainer @tab-select="(e: any) => activeTab = e.detail.tab.dataset.key">
-      <ui5-tab data-key="explore" text="Explore" selected></ui5-tab>
-      <ui5-tab data-key="sql" text="SQL"></ui5-tab>
-      <ui5-tab data-key="dashboard" text="Dashboard"></ui5-tab>
+    <ui5-tabcontainer @tab-select="onTabSelect">
+      <ui5-tab data-key="explore" text="Explore" icon="chart-table-view" selected></ui5-tab>
+      <ui5-tab data-key="sql" text="SQL" icon="syntax"></ui5-tab>
+      <ui5-tab data-key="dashboard" text="Dashboard" icon="dashboard"></ui5-tab>
     </ui5-tabcontainer>
     <div class="tab-content">
-      <p>Active tab: {{ activeTab }}</p>
+      <ExploreTab v-if="activeTab === 'explore'" />
+      <SqlTab v-else-if="activeTab === 'sql'" />
+      <DashboardTab v-else-if="activeTab === 'dashboard'" />
     </div>
   </div>
 </template>
@@ -29,5 +38,6 @@ const activeTab = ref('explore')
 .tab-content {
   flex: 1;
   margin-top: 1rem;
+  overflow: hidden;
 }
 </style>
